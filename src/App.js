e8cf0c62ea7f2f370c04657ffeb1745a148cd8b5
SuprijaDevi <ropotther@gmail.com>
2024-06-19 22:22:16 +0530

    2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Home from './Components/Navbar/Home/Home';
 import Navbar from './Components/Navbar/Navbar';
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <Navbar />
+      <Home />
     </div>
   );
 }
--- src/Components/Navbar/Home/Home.jsx
+++ src/Components/Navbar/Home/Home.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import Sphere from 'E:/portfolioo/src/assets/1-unscreen.gif';
+import './Home.css';
 
 const Home = () => {
   return (
-    <div>
-      
+    <div className='home'>
+      <img src={Sphere} className="gif-image" alt="Animated sphere"></img>
+      <h2>I am Suprija Devi</h2>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
+
