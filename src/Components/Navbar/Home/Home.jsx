1613527b5e18bc4073c76febfe7e4f53324544f1
SuprijaDevi <ropotther@gmail.com>
2024-06-20 12:34:03 +0530

    2

--- src/Components/Navbar/Home/Home.jsx
+++ src/Components/Navbar/Home/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { ReactTyped } from 'react-typed';
 import Sphere from 'E:/portfolioo/src/assets/1-unscreen.gif';
 import './Home.css';
 
@@ -6,10 +7,20 @@ const Home = () => {
   return (
     <div className='home'>
       <img src={Sphere} className="gif-image" alt="Animated sphere"></img>
-      <h2>I am Suprija Devi</h2>
+      <div className='animated-typing'>
+        <ReactTyped
+          strings={[
+            "I'm Suprija Devi",
+            "I'm a Frontend Developer.",
+            "I'm an Enthusiastic Learner.",
+          ]}
+          typeSpeed={100}
+          backSpeed={60}
+          loop
+        />
+      </div>
     </div>
   );
 }
 
 export default Home;
-
