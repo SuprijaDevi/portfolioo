df0f531b73253ee8b82b310dbc03b8537799821d
SuprijaDevi <ropotther@gmail.com>
2024-06-26 19:44:26 +0530

    8

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
-import './App.css'
-import About from './Components/About/About'
-import Achieve from './Components/Achievements/Achieve'
-import Home from './Components/Home/Home'
-import Navbar from './Components/Navbar/Navbar'
-import Projects from './Components/Projects/Projects'
-import Contact from './Components/Contact/Contact'
+import './App.css';
+import About from './Components/About/About';
+import Achieve from './Components/Achievements/Achieve';
+import Home from './Components/Home/Home';
+import Navbar from './Components/Navbar/Navbar';
+import Projects from './Components/Projects/Projects';
+import Contact from './Components/Contact/Contact';
+import { useEffect, useState } from 'react';
+import { motion } from 'framer-motion';
 
 function App() {
+  const [mousePosition, setMousePosition] = useState({
+    x: 0,
+    y: 0
+  });
+
+  useEffect(() => {
+    const mouseMove = e => {
+      setMousePosition({
+        x: e.clientX,
+        y: e.clientY
+      });
+    };
+    window.addEventListener("mousemove", mouseMove);
+    return () => {
+      window.removeEventListener("mousemove", mouseMove);
+    };
+  }, []);
+
+  const variants = {
+    default: {
+      x: mousePosition.x,
+      y: mousePosition.y
+    }
+  };
+
   return (
     <div className="App">
       <Navbar />
@@ -15,8 +42,13 @@ function App() {
       <Projects />
       <Achieve />
       <Contact />
+      <motion.div 
+        className='cursor'
+        variants={variants}
+        animate="default"
+      />
     </div>
   );
 }
 
-export default App
+export default App;
