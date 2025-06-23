2ca703df9a2c6951b8dfdc53e20ce93c247990ab
SuprijaDevi <ropotther@gmail.com>
2025-06-23 16:01:21 +0530

    navbar toggle

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,18 +1,25 @@
-import React from 'react'
-import './Navbar.css'
+import React, { useState } from 'react';
+import './Navbar.css';
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <div className='navbar'>
-        <span className='nav-name'>Suprija Devi</span>
-      <ul>
-        <li><a href='#homee'>Home</a></li>
-        <li><a href='#aboutt'>About me</a></li>
-        <li><a href='#projectt'>Projects</a></li>
-        <li><a href='#contactt'>Contact</a></li>
+      <span className='nav-name'>Suprija Devi</span>
+
+      <div className="toggle-menu" onClick={() => setMenuOpen(!menuOpen)}>
+        {menuOpen ? '⤫' : '☰'}
+      </div>
+
+      <ul className={`nav-links ${menuOpen ? 'active' : ''}`}>
+        <li onClick={() => setMenuOpen(false)}><a href='#homee'>Home</a></li>
+        <li onClick={() => setMenuOpen(false)}><a href='#aboutt'>About me</a></li>
+        <li onClick={() => setMenuOpen(false)}><a href='#projectt'>Projects</a></li>
+        <li onClick={() => setMenuOpen(false)}><a href='#contactt'>Contact</a></li>
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
